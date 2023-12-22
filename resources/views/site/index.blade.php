88188e7f30becbafe929c4183b553ef82b84e533
Joshua Jayrous <111858219+JayrousJr@users.noreply.github.com>
2023-12-22 11:47:30 +0300

    commits

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -11,15 +11,13 @@
                 <div class="row justify-content-center ">
 
                     <div class="col-lg-6 text-center ">
-                        <a href="#" href="https://vimeo.com/342333493" data-fancybox data-aos="fade-up" data-aos-delay="0" class="caption mb-4 d-inline-block">Watch the video</a>
+                        <!-- <a href="#" href="https://vimeo.com/342333493" data-fancybox data-aos="fade-up" data-aos-delay="0" class="caption mb-4 d-inline-block">Watch the video</a> -->
 
-                        <h1 class="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Education is
-                            the Mother of Leadership</h1>
+                        <h1 class="mb-4 heading text-white" data-aos="fade-up" data-aos-delay="100">Education for
+                            Brigter Future</h1>
                         <p class="mb-0" data-aos="fade-up" data-aos-delay="300"><a href="#" class="btn btn-secondary">Explore courses</a></p>
 
                     </div>
-
-
                 </div>
 
             </div>
