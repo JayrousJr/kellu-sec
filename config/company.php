8be779d142c4be320d9cf9e18cf26b07fae76325
Joshua Jayrous <111858219+JayrousJr@users.noreply.github.com>
2023-12-22 11:26:45 +0300

    commint

---
+++ config/company.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+    'name'  =>  'TechClouds',
+    'link' => 'https://cloudstechn.com/',
+    'address' => [
+        'street' => 'Mlimba',
+        'city' => 'Morogoro',
+        'country' => 'Tanzania'
+    ],
+    'email' => 'info@kellusec.com',
+    'phone1' => '+255 752 870 339',
+    'phone2' => '+255 755 019 634',
+    'phone3' => '+255 782 241 030',
+    'phone4' => '+255 673 947 598',
+    'description' => 'Techclouds Provides the best and outstanding technological services such as web designing and web development, graphics design, multimedia and network security to always make you in the best position to attract your customers and for better growth. Welcome for more!'
+];
