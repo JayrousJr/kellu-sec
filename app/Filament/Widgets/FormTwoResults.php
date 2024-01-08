5c0891f0fd20a5dbb50cd05a57441469e2016e5e
Joshua Jayrous <111858219+JayrousJr@users.noreply.github.com>
2024-01-08 12:23:52 +0300

    Update FormTwoResults.php

--- app/Filament/Widgets/FormTwoResults.php
+++ app/Filament/Widgets/FormTwoResults.php
@@ -10,8 +10,30 @@ class FormTwoResults extends ChartWidget
 
     protected function getData(): array
     {
+        $visitors = FormTwoResults::select('created_at', 'id')->get()->groupBy(function ($Visitor) {
+            return FormTwoResults::parse($Visitor->created_at)->format('M');
+        });
+        $VisitorCount = [];
+        foreach ($visitors as $oneVisitor => $VisitorGroup) {
+            $VisitorCount[$oneVisitor] = $VisitorGroup->count();
+        }
+        $labels = array_keys($VisitorCount);
+        $data = array_values($VisitorCount);
         return [
-            //
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Visitors',
+                    'data' => $data,
+                    'fill' => [
+                        'target' => 'origin',
+                        'below' => 'rgba(54, 162, 235, 0.2)',
+                        'above' => 'rgba(54, 162, 235, 0.2)',
+                    ],
+                    'borderColor' => 'rgba(54, 162, 235, 0.7)',
+                    'tension' => 0.5,
+                ],
+            ],
         ];
     }
 
